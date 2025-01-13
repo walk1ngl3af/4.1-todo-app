472a35e01f02c458ff4a12b90597426f3e493210
s3hw <s3hw009@gmail.com>
2025-01-13 09:21:27 -0800

    abc

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  let toDoItems = [
+    {'id' : 1, 'description': 'wash dishes', 'completed': false},
+    {'id' : 2, 'description': 'fold laundry', 'completed': true},
+    {'id' : 3, 'description': 'do homework', 'completed': false},
+  ]
+  res.render('index', {toDoItems});
 });
 
 module.exports = router;
